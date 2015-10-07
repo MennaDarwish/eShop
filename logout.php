2dff45bbf5cfeb8aaec8d289bee37f941a2e75ac
Menna Darwish <mennadarwish1@gmail.com>
2015-10-07 13:39:02 +0200

    modified logout.php

--- logout.php
+++ logout.php
@@ -4,11 +4,11 @@
 		header("Location: index.php");
 	}
 	else if (isset($_SEESION['user'])!="") {
-		header("Location: index.php")
+		header("Location: index.php");
 	}
 	if(isset($_GET['logout'])) {
 		session_destroy();
 		unset($_SEESION['user']);
-		header("Location: index.php")
+		header("Location: index.php/loggedOut");
 	}
 ?>
