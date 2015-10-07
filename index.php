5d62e077dd9936ff75f095dae70cd8b06a22d1ec
Menna Darwish <mennadarwish1@gmail.com>
2015-10-07 02:41:13 +0200

    added add-to-cart functionality

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+	session_start();
+	include_once 'dbconnect.php';
+
+	if(!isset($_SESSION['user'])) {
+		header("Location:login.php");
+	}
+	$res=mysql_query("SELECT * FROM User WHERE id=".$_SESSION['user']);
+	$userRow=mysql_fetch_array($res);
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -22,7 +32,14 @@
 			<div class="vertical-line"></div>
 			</div>
 			<div class="navbar-content">
-			<div class="navbar-register">Register</div>
+			<!--div class="navbar-register">Register</div-->
+			<div class="navbar-content">
+				<div class="navbar-name">
+					<?php
+						echo $userRow['firstName'], $userRow['lastName']
+					?>
+				</div>
+			</div>
 			</div>
 			<!--div class="navbar-content"><div class="navbar-register">Register</div></div-->
 		</div>
@@ -36,21 +53,47 @@
 			<div class="product-title">OUR WEAPONS</div>
 		
 			<?php
-				mysql_connect("localhost", "root", "") or die (mysql_error());
-				mysql_select_db('eShop') or die (mysql_error());
 
+				include_once 'dbconnect.php';
+				if (isset($_SESSION['user'])) {
+					$user = $_SESSION['user'];
+				}
+				if (!empty($_GET['id']) AND !isset($_SESSION['user'])) {
+					header ("Location: login.php");
+				}	
+					$product_id = isset($_GET['id']) ? $_GET['id'] : "";
+					$cart = mysql_query("SELECT * FROM Cart WHERE product_id='$product_id' AND user_id='$user'");
+					$num_of_rows = mysql_num_rows($cart);
+					if($num_of_rows == 0 ) {
+						mysql_query("INSERT INTO Cart(product_id, user_id) VALUES('$product_id', '$user')");
+					}
+					else {
+						$cart_row = mysql_fetch_array($cart);
+						//$inc_quantity = $cart_row[product_quantity]+1;
+						//mysql_query("UPDATE Cart SET product_quantity='$inc_quantity' WHERE product_id='$product_id' AND user_id='$userRow'");
+					}
+				
+				//List all products
 				$query_products = "SELECT * FROM Product ORDER BY name";
 				$products = mysql_query($query_products) or die (mysql_error());
 				while($row = mysql_fetch_assoc($products))
 				{
 					if($row >0){
-
-					echo "<div class='product-block-1'>{$row['name']} {$row['price']} 
-					<img class='img' src='images/{$row['image']}'>
-					</div>";
-				
-
-					} else 
+							
+					echo"<div class='product-block-1'> 
+									<a href='index.php?id={$row['id']}'>ADD TO CART</a>
+									<div class='product-name'>{$row['name']}
+										<div class='product-price'> {$row['price']} </div>
+										<div class= 'product-image'> 
+											<img class='img' src='images/{$row['image']}'>
+										</div>
+										<div class='product-stock'>{$row['stock']}</div>
+										<input type='hidden' name='product_id' value='{$row['id']}'/>
+										<input type='submit' value='ADD TO CART' class='add-to-cart-btn'/>
+								</div>
+								</div>";
+				}
+					 else 
 					{
 						echo '<p>no product.</p>';
 					}
