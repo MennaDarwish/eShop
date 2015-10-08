0a7ba46db80a5188cf1d2b3dc8745dedf42b5e38
Menna Darwish <mennadarwish1@gmail.com>
2015-10-08 08:01:38 +0200

    modified register view

--- register.php
+++ register.php
@@ -25,28 +25,47 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="stylesheet/style.css">
+	<link rel="stylesheet" type="text/css" href="stylesheet/register.css">
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/script.js"></script>
+
 	<title></title>
 </head>
 <body>
-		<form method='post'>
+		<div class="main-container">
+		<div class="navbar-container">
+			<div class="home">
+				<a href="index.php"> HOME </a>
+			</div>			
+		</div>
+		<div class="main-container">
+		<div class="register-info-container">
+		<div class="container">
+		<form  method='post'>
+			<div class= "fname">
 			<label class="register-fname-label">First Name:</label>
 			<input id="register-fname" placeholder="First Name" type="text" name="fname">
-			
+			</div>
+			<div class='lname'>
 			<label class="register-lname-label">Last Name:</label>
 			<input id="register-lname" placeholder="Last Name" type="text" name="lname">
-			
+			</div>
+			<div class='email'>
 			<label class="register-email-label">Email:</label>
 			<input id="register-email" placeholder="Email" type="text" name="email">
-			
+			</div>
+			<div class="pass">
 			<label class="register-pass-label">Password:</label>
 			<input id="register-pass" placeholder="********" type="password" name="password">
-			
+			</div>
 			<button class="signup-btn" type="submit" name="btn-signup">Register</button>
 
-			<a href="login.php">Sign in here</a>
+			<a class="signin-anchor"href="login.php">Sign in here</a>
+			<div class="horizontal-line"></div>
 		</form>
+		</div>
+		</div>
+		</div>
+		</div>
 </body>
 </html>
